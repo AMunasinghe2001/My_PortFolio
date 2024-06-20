b070639022577c1621009b16788bfe3842e0d486
Anushangha Munasinghe <153182937+AMunasinghe2001@users.noreply.github.com>
2024-06-21 01:15:37 +0530

    Update addproject.js

--- frontend/src/Components/AddProject/addproject.js
+++ frontend/src/Components/AddProject/addproject.js
@@ -32,7 +32,7 @@ function AddProject() {
         formData.append("image", image);
 
         try {
-            await axios.post("https://anushanga_munasinghe.vercel.app/projects", formData, {
+            await axios.post("my-portfolio-api-two.vercel.app/projects", formData, {
                 headers: {
                     "Content-Type": "multipart/form-data",
                 },
