6c645ae0d20f3403dca6f42d235253c6799966b9
Kaushan M.K.A <it22569622@my.sliit.lk>
2024-06-23 17:51:51 +0530

    edit cv

--- frontend/src/Components/Home/home.js
+++ frontend/src/Components/Home/home.js
@@ -79,7 +79,7 @@ function Home() {
           </div>
 
           <button className="btnCV animated-text">
-            <a href="/Anushanga Munasinge CV.pdf" download>
+            <a href="/Anushanga Munasinghe CV.pdf" download>
               Download Resume <i className="fas fa-download"></i>
             </a>
           </button>
